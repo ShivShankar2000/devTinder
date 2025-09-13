415ea8eb5645c23db4c34800f33126241062a722
ShivShankar2000 <shiv16941@gmail.com>
2025-09-14 01:48:27 +0530

    saving data to database (init database)

--- src/app.js
+++ src/app.js
@@ -2,32 +2,27 @@ const express = require('express');
 const connectDB = require('./config/database');
 
 const app = express();
-const {userAuth, adminAuth} = require('./middlewares/auth');
-app.use("/user", userAuth);
+const {userAuth} = require('./models/user');
+const User = require('./models/user');
 
-app.get("/user",(req, res )=>{
-    console.log(req.query , req.params);
-    res.send({name: "Shiv", age: 24, city: "Kolkata"})
-})
-
-app.get("/admin", adminAuth,(req, res)=>{
+app.post("/signup", async(req, res )=>{
+    const userObj = {
+        firstName : "Shivam",
+        lastName : "Kumar",
+        emailId : "shivam.kumar@example.com",
+        password : "password123",
+        age : "24",
+        gender : "Male"
+    }
+    const user= new User(userObj);
     try{
-        throw new Error("Some error")
-    }catch(e){
-        res.status(500).send("Contact support team !")
+        await user.save();
+        res.status(200).send(user);
+    }catch(err){
+        res.status(500).send("Internal Server Error");
     }
 })
 
-app.get("/admin/:id",(req, res , next)=>{
-    console.log(req.query , req.params);
-    res.send("Admin route with ID "+ req.params.id)
-})
-
-app.use("/",(err, req, res, next)=>{
-    if(err) {
-    res.status(500).send("Something went wrong!")
-    }
-})
 connectDB().then(()=>{
     console.log("DB connected");
     app.listen(3000, ()=>{
