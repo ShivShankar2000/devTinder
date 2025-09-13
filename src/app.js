67a3301ee1523c074ff9de37cdc7414c528fbb82
ShivShankar2000 <shiv16941@gmail.com>
2025-09-14 00:25:09 +0530

    work of middleware

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use("/user",(req, res, next)=>{
+    const token = "abc"
+    const isAuthorized = token === "xyz"
+    if(!isAuthorized){
+        return res.status(401).send("Unauthorized!")
+    }else{
+        next()
+    }
+})
 
-app.get("/user/:userId",(req, res)=>{
+app.get("/user",(req, res , next)=>{
     console.log(req.query , req.params);
-    
     res.send({name: "Shiv", age: 24, city: "Kolkata"})
 })
 
@@ -16,14 +24,6 @@ app.post("/user",(req, res)=>{
 app.delete("/user",(req, res)=>{
     res.send({status: "User deleted successfully", userId: 1})
 })
-
-// app.use("/test",(req, res)=>{
-//     res.send("Test route...")
-// })
-
-// app.use("/",(req, res)=>{
-//     res.send("Namaste Shiv...")
-// })
 
 app.listen(3000, ()=>{
     console.log("Running on port 3000")
