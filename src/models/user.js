4a5557e20f654680099664c1e7b2ce0917722b32
ShivShankar2000 <shiv16941@gmail.com>
2025-09-17 21:43:30 +0530

    explore validator library

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String },
-    emailId: { type: String, required: true, unique: true, match: /.+\@.+\..+/, lowercase: true, trim: true },
-    password: { type: String, required: true, minLength: 6, maxLength: 64, trim: true },
+    emailId: { type: String, required: true, unique: true, match: /.+\@.+\..+/, lowercase: true, trim: true ,validate(value) {
+        if (!validator.isEmail(value)) {
+            throw new Error('Invalid email address: ' + value);
+        }
+    }},
+    password: { type: String, required: true, minLength: 6, maxLength: 64, trim: true , validate(value) {
+        if (!validator.isStrongPassword(value, { minLength: 6, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 })) {
+            throw new Error('Password is not strong enough: ' + value);
+        }
+    }   },
     age: { type: Number, min: 18, max: 100 },
     gender: { type: String, enum: ['male', 'female', 'other', 'Male', 'Female', 'Other'], trim: true,
         validate(value) { 
@@ -13,7 +22,11 @@ const userSchema = new mongoose.Schema({
             } 
         } 
     },
-    photoUrl: { type: String, default: "https://www.pngall.com/wp-content/uploads/5/Profile-PNG-High-Quality-Image.png" },
+    photoUrl: { type: String, default: "https://www.pngall.com/wp-content/uploads/5/Profile-PNG-High-Quality-Image.png", validate(value) {
+        if (!validator.isURL(value)) {
+            throw new Error('Invalid URL: ' + value);
+        }
+    } },
     bio: { type: String, maxLength: 250, default: "Hello! I am using DevTinder." },
     skills: { type: [String], unique: true, trim: true},
 },{
