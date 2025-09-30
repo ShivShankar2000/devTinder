974a839c8d23b85fcadd12cc8ab3af59b57e4bde
ShivShankar2000 <shiv16941@gmail.com>
2025-10-01 00:20:05 +0530

    feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const router = express.Router();
 
@@ -48,6 +49,45 @@ router.get("/user/connection", userAuth, async (req, res) => {
     res.json({
       message: "User connections retrieved successfully",
       data: data,
+    });
+  } catch (error) {
+    res.status(400).send("Error :" + error.message);
+  }
+});
+
+router.get("/user/feeds", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    const userId = user._id;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit; // Max limit is 50
+    const skip = (page - 1) * limit;
+
+    // Find all users the current user is connected to
+    const connections = await ConnectionRequest.find({
+      $or: [{ fromUserId: userId }, { toUserId: userId }],
+    })
+      .select("fromUserId toUserId")
+      .skip(skip)
+      .limit(limit);
+    const hideUsersFomFeeds = new Set();
+    connections.forEach((conn) => {
+      hideUsersFomFeeds.add(conn.fromUserId.toString());
+      hideUsersFomFeeds.add(conn.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $ne: userId } }, // Exclude the current user
+        { _id: { $nin: Array.from(hideUsersFomFeeds) } }, // Exclude connected users
+      ],
+    }).select("firstName lastName photoUrl age gender skills bio");
+
+    res.json({
+      message: "User feeds retrieved successfully",
+      data: users,
     });
   } catch (error) {
     res.status(400).send("Error :" + error.message);
